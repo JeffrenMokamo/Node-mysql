e8268bb6f0d437f31b0713716f08b2e9b04de716
Jeffren Mokamo <jeffrenmokamo726@gmail.com>
2023-11-24 21:51:27 +0100

    Installation de bootstrap en cdn pour construire les interafces utilisateurs

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+// Importation des packages node js
 const express = require('express');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
 const path = require('path');
 
+// l'initialisation d'express
 const app = express();
 
+// Configuration du fichier .env
 dotenv.config({
     path: './.env'
 })
 
 const port = process.env.PORT || 5000;
 
+// Connection avec la base des données
 const dbConnect = mysql.createConnection({
     host: 'localhost',
     database: process.env.DATABASE,
@@ -18,6 +22,7 @@ const dbConnect = mysql.createConnection({
     password: ''
 })
 
+// Vérification de la connection avec la base de données 
 dbConnect.connect((err) => {
     if (err) {
         console.log(err);
@@ -27,15 +32,22 @@ dbConnect.connect((err) => {
     }
 })
 
+// On signale à express de considérer le dossier public 
 const publicDir = path.join(__dirname, './public');
 app.use(express.static(publicDir));
 
 app.set('view engine', 'hbs');
 
+// Le fichier index.hbs est envoyé à la racine de notre projet express
 app.get("/", (req, res) => {
     res.render("index");
 })
 
+app.get("/inscription", (req, res) => {
+    res.render("inscription");
+})
+
+//Lancement du serveur
 app.listen(port, () => {
     console.log(`Le serveur est lancé dans le port ${port}`);
 })
